6c9f8da8c8027500e541d4cf784f5eb58cdf198e
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2024-06-25 15:51:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// synchronous
+
+console.log(" I ");
+console.log(" eat ");
+console.log(" spoon ");
+console.log(" ice cream");
+console.log(" with a ");
+
+// asynchronous
+console.log(" I ");
+console.log(" eat ");
+
+setTimeout(() => {
+    console.log(" ice cream");
+},4000)
+
+console.log(" with a ");
+console.log(" spoon ");
+
+
+
